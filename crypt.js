1491c8d1eff57a234d35c438c80ae4bad6f54193
Max1Truc <Max1Truc@users.noreply.github.com>
2017-04-07 12:39:04 +0200

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -7,7 +7,7 @@ function crypt (message, password, uncrypt=false) {
     password = password.join("")
     var extended_password = extended_password + password;
     for (i = 0; i < message.length; i++) {
-        if (!uncrypt} {
+        if (!uncrypt) {
             message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
         } else {
             message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) - alphanumeric_chars.indexOf(extended_password[i])];
