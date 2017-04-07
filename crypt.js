bd6fedbae17308913b0e6d0748c418291f84a50b
Max1Truc <Max1Truc@users.noreply.github.com>
2017-04-07 11:21:05 +0200

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -1,9 +1,13 @@
 function crypt (message, password, uncrypt=false) {
-    extended_password = ((message.length - (message.length % password.length))/password.length) * password;
-    extended_password += password[:password.length-(message.length % password.length)];
+    var extended_password = ((message.length - (message.length % password.length))/password.length) * password;
+    var extended_password += password[:password.length-(message.length % password.length)];
     
     for (i = 0; i < message.length; i++) {
-        message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
+        if (!uncrypt} {
+            message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
+        } else {
+            message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) - alphanumeric_chars.indexOf(extended_password[i])];
+        };
     };
     return message;
 };
