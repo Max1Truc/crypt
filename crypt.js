41a32ca0cf72c9226108e425a1e57166c63287ac
Max1Truc <Max1Truc@users.noreply.github.com>
2017-04-07 13:04:41 +0200

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -1,21 +1,22 @@
 function crypt (message, password, uncrypt=false) {
     
-	channel = "DEFAULT";
-	alphanumeric_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789àâéêèëîïôöçù',!?.;:=+-()/@%$*? ";
+    window.message = message;
+    window.password = password
+    alphanumeric_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789àâéêèëîïôöçù',!?.;:=+-()/@%$*? ";
     
     var extended_password = ((message.length - (message.length % password.length))/password.length) * password;
-    var password = password.split("");
+    window.password = window.password.split("");
     for (i = 0; i < password.length-(message.length % password.length); i++) {
-        var tmp = password.pop();
+        var tmp = window.password.pop();
     };
-    password = password.join("")
+    var password = window.password.join("");
     var extended_password = extended_password + password;
     for (i = 0; i < message.length; i++) {
         if (!uncrypt) {
-            message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
+            window.message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
         } else {
-            message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) - alphanumeric_chars.indexOf(extended_password[i])];
+            window.message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) - alphanumeric_chars.indexOf(extended_password[i])];
         };
     };
-    return message;
+    return window.message;
 };
