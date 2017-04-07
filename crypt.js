237632bae750a6b32897f61a0f2d4e713f951f31
Max1Truc <Max1Truc@users.noreply.github.com>
2017-04-07 13:55:56 +0200

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -1,21 +1,21 @@
 function crypt (message, password, uncrypt=false) {
     
-    window.message = message;
-    window.password = password
-    alphanumeric_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789àâéêèëîïôöçù',!?.;:=+-()/@%$*? ";
+    const alphanumeric_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789àâéêèëîïôöçù',!?.;:=+-()/@%$*? ";
     
-    var extended_password = ((message.length - (message.length % password.length))/password.length) * password;
-    window.password = window.password.split("");
-    for (i = 0; i < password.length-(message.length % password.length); i++) {
-        var tmp = window.password.pop();
+    let message = message;
+    let password = password;
+    let extended_password = ((message.length - (message.length % password.length))/password.length) * password;
+    password = password.split("");
+    for (let i = 0; i < password.length-(message.length % password.length); i++) {
+        var tmp = password.pop();
     };
-    var password = window.password.join("");
-    var extended_password = extended_password + password;
-    for (i = 0; i < message.length; i++) {
+    password = password.join("");
+    extended_password = extended_password + password;
+    for (let i = 0; i < message.length; i++) {
         if (!uncrypt) {
-            window.message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
+            message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i])];
         } else {
-            window.message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) - alphanumeric_chars.indexOf(extended_password[i])];
+            message[i] =  alphanumeric_chars[alphanumeric_chars.indexOf(message[i]) - alphanumeric_chars.indexOf(extended_password[i])];
         };
     };
     return window.message;
