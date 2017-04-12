209683c65750e4c42ee20c4db6cd64e51ab2ef75
Max1Truc <Max1Truc@users.noreply.github.com>
2017-04-12 12:45:00 +0200

    Update and rename crypt.browser.js to crypt.js

--- crypt.js
+++ crypt.js
@@ -2,8 +2,6 @@
 // Source available on GitHub at https://GitHub.com/Max1Truc/crypt
 // It's under the MIT License
 
-document.write("<p id='temp_global'></p>");
-
 // All is in the var name
 const alphanumeric_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\"',!.;:=+-()/\\@%$*? ".repeat(2);
 
@@ -11,30 +9,24 @@ function crypt (message, password) {
     
     let extended_password = ((message.length - (message.length % password.length))/password.length) + 1;
     extended_password = password.repeat(extended_password);
-	
+    let crypted = "";
 	
     for (let i = 0; i < message.length; i++) {
 		index_in_alphanumeric_chars = alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i]);
-		document.getElementById("temp_global").innerHTML += alphanumeric_chars[index_in_alphanumeric_chars];
+		crypted = crypted.concat(alphanumeric_chars[index_in_alphanumeric_chars]);
     };
-	
-    let crypted = document.getElementById("temp_global").innerHTML;
-    document.getElementById("temp_global").innerHTML = "";
     return crypted;
 };
 
-function uncrypt (crypted_message, password) {
+function decrypt (crypted_message, password) {
 	
     let extended_password = ((crypted_message.length - (crypted_message.length % password.length))/password.length) + 1;
     extended_password = password.repeat(extended_password);
-	
+    let decrypted = "";
 	
     for (let i = 0; i < crypted_message.length; i++) {
 	index_in_alphanumeric_chars = (alphanumeric_chars.indexOf(crypted_message[i]) - alphanumeric_chars.indexOf(extended_password[i])) + (alphanumeric_chars.length / 2);
-	document.getElementById("temp_global").innerHTML += alphanumeric_chars[index_in_alphanumeric_chars];
+	decrypted = decrypted.concat(alphanumeric_chars[index_in_alphanumeric_chars]);
     };
-	
-    let crypted = document.getElementById("temp_global").innerHTML;
-    document.getElementById("temp_global").innerHTML = "";
-    return crypted;
+    return decrypted;
 };
