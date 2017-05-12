98b8824d8ec6e12dda91d8da9929ac10b316bb63
Max1Truc <Max1Truc@users.noreply.github.com>
2017-05-12 20:18:47 +0200

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -2,10 +2,7 @@
 // Source available on GitHub at https://GitHub.com/Max1Truc/crypt
 // It's under the MIT License
 
-// All is in the var name
-var alphanumeric_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789\"',!.;:=+-()/\\@%$*? ".repeat(2);
-
-function crypt (message, password) {
+function crypt (message, password, decrypt=false) {
     // to make the password string longer
     var extended_password = ((message.length - (message.length % password.length))/password.length) + 1;
     extended_password = password.repeat(extended_password);
@@ -15,27 +12,15 @@ function crypt (message, password) {
 	
     // crypt each letter
     for (var i = 0; i < message.length; i++) {
-		index_in_alphanumeric_chars = alphanumeric_chars.indexOf(message[i]) + alphanumeric_chars.indexOf(extended_password[i]);
-		crypted = crypted.concat(alphanumeric_chars[index_in_alphanumeric_chars]);
+		var index_text_letter = message[i].charCodeAt(0),
+		    index_pass_letter = extended_password[i].charCodeAt(0),
+		    crypted_letter = String.fromCharCode(!decrypt?index_text_letter+index_pass_letter:index_text_letter-index_pass_letter);
+		crypted = crypted.concat(crypted_letter);
     };
     // return
     return crypted;
 };
 
 function decrypt (crypted_message, password) {
-    
-    // to make the password string longer
-    var extended_password = ((crypted_message.length - (crypted_message.length % password.length))/password.length) + 1;
-    extended_password = password.repeat(extended_password);
-    
-    // initialize the decrypted message var
-    var decrypted = "";
-
-    // decrypt each letter
-    for (var i = 0; i < crypted_message.length; i++) {
-	index_in_alphanumeric_chars = (alphanumeric_chars.indexOf(crypted_message[i]) - alphanumeric_chars.indexOf(extended_password[i])) + (alphanumeric_chars.length / 2);
-	decrypted = decrypted.concat(alphanumeric_chars[index_in_alphanumeric_chars]);
-    };
-    // return
-    return decrypted;
+    return crypt(crypted_message, password, decrypt=1);
 };
