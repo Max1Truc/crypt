5e53a2be0c9d5bdde29241c83fbfc6a463e5f8c8
Max1Truc <Max1Truc@users.noreply.github.com>
2018-01-28 22:20:48 +0100

    Added test function and verified code with ESLint

--- crypt.js
+++ crypt.js
@@ -2,30 +2,41 @@
 // Source available on GitHub at https://GitHub.com/Max1Truc/crypt
 // It's under the MIT License
 
-function crypt (message, password) {
-    // initialize the crypted message var
-    var crypted = "";
-    // crypt each letter
-    for (var i = 0; i < message.length; i++) {
-	var index_text_letter = message[i].charCodeAt(0),
-	    index_pass_letter = password[i%password.length].charCodeAt(0);
-	var crypted_letter = String.fromCharCode(index_text_letter+index_pass_letter);
-	crypted = crypted.concat(crypted_letter);
-    };
-    // return
-    return crypted;
-};
+'use strict'
 
-function decrypt (crypted_message, password) {
-    // initialize the decrypted message var
-    var decrypted = "";
-    // decrypt each letter
-    for (var i = 0; i < crypted_message.length; i++) {
-	var index_text_letter = crypted_message[i].charCodeAt(0),
-	    index_pass_letter = password[i%password.length].charCodeAt(0);
-	var decrypted_letter = String.fromCharCode(index_text_letter-index_pass_letter);
-	decrypted = decrypted.concat(decrypted_letter);
-    };
-    // return
-    return decrypted;
-};
+function crypt (message, password) {
+  // initialize the crypted message var
+  var crypted = ''
+  // crypt each letter
+  for (var i = 0; i < message.length; i++) {
+    var indexLetterInText, indexLetterInPass, cryptedLetter
+    indexLetterInText = message[i].charCodeAt(0)
+    indexLetterInPass = password[i % password.length].charCodeAt(0)
+    cryptedLetter = String.fromCharCode(indexLetterInText + indexLetterInPass)
+    crypted = crypted.concat(cryptedLetter)
+  };
+  // return
+  return crypted
+}
+
+function decrypt (cryptedMessage, password) {
+  // initialize the decrypted message var
+  var decrypted = ''
+  // decrypt each letter
+  for (var i = 0; i < cryptedMessage.length; i++) {
+    var indexLetterInText, indexLetterInPass, decryptedLetter
+    indexLetterInText = cryptedMessage[i].charCodeAt(0)
+    indexLetterInPass = password[i % password.length].charCodeAt(0)
+    decryptedLetter = String.fromCharCode(indexLetterInText - indexLetterInPass)
+    decrypted = decrypted.concat(decryptedLetter)
+  };
+  // return
+  return decrypted
+}
+
+// Tests functions, else log it into the console
+var message = 'Hello World !'
+var pass = 'MyPassword'
+if (decrypt(crypt(message, pass), pass) !== message) {
+  console.error('Error in Max1Truc\'s Crypt library !')
+}
